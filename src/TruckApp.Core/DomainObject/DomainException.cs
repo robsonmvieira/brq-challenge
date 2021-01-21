3d341adefd6d33baaedc66f560c4a94754156424
Robson Maia <robsonmvieira@gmail.com>
2021-01-21 10:57:19 -0300

    :sparkles: add DomainException.cs

---
+++ src/TruckApp.Core/DomainObject/DomainException.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace TruckApp.Core.DomainObject
+{
+    public class DomainException: Exception
+    {
+        public DomainException()
+        {
+            
+        }
+        
+        public DomainException(string message): base(message)
+        {
+            
+        }
+        
+        public DomainException(string message, Exception innerException): base(message, innerException)
+        {
+            
+        }
+        
+        
+    }
+}
