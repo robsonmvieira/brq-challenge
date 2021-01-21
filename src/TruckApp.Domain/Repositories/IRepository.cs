e8fe53fd4dbab900b297089401758c7f09b42023
Robson Maia <robsonmvieira@gmail.com>
2021-01-20 21:56:20 -0300

    :sparkles: Add Generic interface repository

---
+++ src/TruckApp.Domain/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TruckApp.Domain.Entities;
+
+namespace TruckApp.Domain.Repositories
+{
+    public interface IRepository<T> where T: Entity
+    {
+        Task Add(T entity);
+        Task<IEnumerable<T>> ListAll();
+        Task<T> GetById(Guid id);
+        Task Update(T entity);
+        Task Remove(T entity);
+    }
+}
