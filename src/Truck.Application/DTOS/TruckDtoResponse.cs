a26d390991b78b8d208caca190c8983e9b3e25a0
Robson Maia <robsonmvieira@gmail.com>
2021-01-21 10:47:48 -0300

    :sparkles: add Truck request and response format

---
+++ src/Truck.Application/DTOS/TruckDtoResponse.cs
@@ -0,0 +1,9 @@
+namespace Truck.Application.DTOS
+{
+    public class TruckDtoResponse
+    {
+        public string Modelo { get; set; }
+        public int AnoFabricacao { get; set; }
+        public int AnoModelo { get;  set; }
+    }
+}
