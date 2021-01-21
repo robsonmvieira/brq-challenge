b0564ee77c10eb6ac275a83a173d6c50de45cfbd
Robson Maia <robsonmvieira@gmail.com>
2021-01-20 21:46:27 -0300

    :hammer: update TruckMapping

--- src/TruckApp.Data/Mapping/TruckMapping.cs
+++ src/TruckApp.Data/Mapping/TruckMapping.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TruckApp.Domain.Entities;
+
 namespace TruckApp.Data.Mapping
 {
-    public class TruckMapping
+    public class TruckMapping: IEntityTypeConfiguration<Truck>
     {
-        
+        public void Configure(EntityTypeBuilder<Truck> builder)
+        {
+            builder.ToTable("Trucks");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Modelo).IsRequired();
+            builder.Property(x => x.AnoFabricacao).IsRequired();
+            builder.Property(x => x.AnoModelo).IsRequired();
+        }
     }
 }
