387679d3120252b4b9c073d46e0ff7a70cbcae20
Robson Maia <robsonmvieira@gmail.com>
2021-01-21 10:46:06 -0300

    :sparkles: add Add Generic Repository

---
+++ src/TruckApp.Data/Repositories/Repository.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TruckApp.Data.Data;
+using TruckApp.Domain.Entities;
+using TruckApp.Domain.Repositories;
+
+namespace TruckApp.Data.Repositories
+{
+    public class Repository<T>: IRepository<T> where T: Entity
+    {
+        private TruckContext _truckContext;
+        private DbSet<T> Query;
+
+        public Repository(TruckContext truckContext)
+        {
+            _truckContext = truckContext;
+            Query = _truckContext.Set<T>();
+        }
+        public async Task Add(T entity)
+        {
+            Query.Add(entity);
+            await _truckContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> ListAll()
+        {
+            return await Query.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T> GetById(Guid id)
+        {
+            return await Query.FindAsync(id);
+        }
+
+        public async Task Update(T entity)
+        {
+            Query.Update(entity);
+            await _truckContext.SaveChangesAsync();
+        }
+
+        public async Task Remove(T entity)
+        {
+            Query.Remove(entity);
+            await _truckContext.SaveChangesAsync();
+        }
+    }
+}
