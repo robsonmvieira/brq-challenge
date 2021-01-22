3d5c4f3bba179135663e21b8996ec0d4a71ae2f1
Robson Maia <robsonmvieira@gmail.com>
2021-01-22 17:46:20 -0300

    :white_check_mark: Add Migrations files

---
+++ src/TruckApp.Data/Migrations/20210122114142_initial-migrate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TruckApp.Data.Migrations
+{
+    public partial class initialmigrate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trucks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Model = table.Column<string>(type: "varchar(2)", nullable: false),
+                    YearManufacture = table.Column<int>(type: "int", nullable: false),
+                    ModelYear = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trucks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trucks");
+        }
+    }
+}
